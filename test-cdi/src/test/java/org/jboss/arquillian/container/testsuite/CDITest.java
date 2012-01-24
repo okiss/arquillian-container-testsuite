bd505bd3444bf42a38e399d2575546f673013472
Karel Piwko <kpiwko@redhat.com>
2012-01-24 10:18:09 +0100

    Removed unused imports

--- test-cdi/src/test/java/org/jboss/arquillian/container/testsuite/CDITest.java
+++ test-cdi/src/test/java/org/jboss/arquillian/container/testsuite/CDITest.java
@@ -1,25 +1,18 @@
 package org.jboss.arquillian.container.testsuite;
 
+import java.io.File;
+
+import javax.inject.Inject;
+
 import junit.framework.Assert;
+
 import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.as.quickstarts.helloworld.HelloService;
-import org.jboss.as.quickstarts.helloworld.HelloWorldServlet;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.EnterpriseArchive;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import sun.tools.jar.resources.jar;
-
-import javax.annotation.Resource;
-import javax.inject.Inject;
-import java.io.File;
-import java.net.URL;
 
 @RunWith(Arquillian.class)
 public class CDITest {
