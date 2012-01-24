0196f17fca33ef327701a4ad19c8937b8e8cf4a9
Karel Piwko <kpiwko@redhat.com>
2012-01-24 10:05:45 +0100

    Fixed mappedName to reflect AS7 naming

--- test-resource/src/test/java/org/jboss/arquillian/container/testsuite/ResourceTest.java
+++ test-resource/src/test/java/org/jboss/arquillian/container/testsuite/ResourceTest.java
@@ -17,7 +17,7 @@ import org.junit.runner.RunWith;
 @RunWith(Arquillian.class)
 public class ResourceTest {
 
-    @Resource(mappedName = "hello")
+    @Resource(mappedName = "java:app/test/HelloService")
     HelloService service;
 
     @Deployment
