0c1465ab5142abad7b89fd1baf90e999222e5634
Oliver Kiss <okiss@redhat.com>
2012-05-18 11:42:17 +0200

    Added EAR test application

---
+++ package-cdi-ear/ejb/src/main/java/org/jboss/as/quickstarts/helloworld/HelloService.java
@@ -0,0 +1,14 @@
+package org.jboss.as.quickstarts.helloworld;
+
+/**
+ * A simple CDI service which is able to say hello to someone
+ *
+ * @author Pete Muir
+ */
+public class HelloService {
+
+    public String createHelloMessage(String name) {
+        return "Hello " + name + "!";
+    }
+
+}
---
+++ package-cdi-ear/web/src/main/java/org/jboss/as/quickstarts/helloworld/HelloWorldServlet.java
@@ -0,0 +1,44 @@
+package org.jboss.as.quickstarts.helloworld;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * <p>
+ * A simple servlet taking advantage of features added in 3.0.
+ * </p>
+ * <p/>
+ * <p/>
+ * The servlet is registered and mapped to /HelloServlet using the {@linkplain WebServlet
+ *
+ * @author Pete Muir
+ * @HttpServlet}. The {@link HelloService} is injected by CDI.
+ * </p>
+ */
+@SuppressWarnings("serial")
+@WebServlet("/HelloWorld")
+public class HelloWorldServlet extends HttpServlet {
+
+    static String PAGE_HEADER = "<html><head /><body>";
+
+    static String PAGE_FOOTER = "</body></html>";
+
+    @Inject
+    HelloService helloService;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println(PAGE_HEADER);
+        writer.println("<h1>" + helloService.createHelloMessage("World") + "</h1>");
+        writer.println(PAGE_FOOTER);
+        writer.close();
+    }
+
+}
